e43e1cbd36f1b41b8c24caf505a20e45b98a4c0f
Taseen Mohammed <taseen.bibi@gmail.com>
2025-09-24 17:19:16 +0000

    environment variabele gedefineerd

--- appwrite_session.py
+++ appwrite_session.py
@@ -1,4 +1,24 @@
+from dotenv import load_dotenv
+from os import environ
+from appwrite.client import Client
+from appwrite.services.storage import Storage
+
+load_dotenv()
+APPWRITE_API_KEY = environ['APPWRITE_KEY']
+APPWRITE_ENDPOINT = environ['APPWRITE_ENDPOINT']
+APPWRITE_PROJECT_ID = environ['APPWRITE_PROJECT_ID']
+APPWRITE_STORAGE_BUCKET_ID = environ["APPWRITE_STORAGE_BUCKET_ID"]
+
+
+def create_name(file_content: str, file_url: str) -> str:
+    # Hier komt een url uit, en dat heeft de volgende format:
+    # <file_url_hash>__<file_content_hash>
+    pass
+
 class AppwriteSession:
     def __init__(self):
-        ...
-
+        self.client = Client().set_endpoint(APPWRITE_ENDPOINT).set_project(APPWRITE_PROJECT_ID).set_key(APPWRITE_API_KEY)
+        self.storage = Storage(self.client)
+    
+    
+    
